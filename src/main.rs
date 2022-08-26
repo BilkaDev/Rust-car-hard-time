4f979598dbe303f966f9f795d6bf9ac75a3e7247
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-08-26 13:12:58 +0200

    finished EP calculateDisselUsageForDistance

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use rocket::serde::json::{json, Value};
 
 
 #[get("/calculateDisselUsageForDistance/<distance>/<year_of_production>/<fuel_usage_per_100km>")]
-fn dissel_usage(distance: u64,year_of_production: u16, fuel_usage_per_100km: u8) -> Value {
-    json!({"test": "test"})
+fn dissel_usage(distance: u32,year_of_production: u32, fuel_usage_per_100km: u32) -> Value {
+    let fuel_usage = distance as f32 / fuel_usage_per_100km as f32;
+    json!({"fuelUsage": fuel_usage})
 }
 
 #[launch]
