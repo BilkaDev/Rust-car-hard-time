c75b0113abb51170f1a75902af923555997a6428
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-08-26 14:07:39 +0200

    Finisged EP proabilityOfUnitInjectorFail

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 use rocket::serde::json::{json, Value};
 use regex::Regex;
+use rand::Rng;
+use rust_decimal::prelude::*;
 
 #[get("/calculateDisselUsageForDistance/<distance>/<year_of_production>/<fuel_usage_per_100km>")]
 fn dissel_usage(distance: u32,year_of_production: u32, fuel_usage_per_100km: u32) -> Value {
@@ -13,7 +15,17 @@ fn dissel_usage(distance: u32,year_of_production: u32, fuel_usage_per_100km: u32
 fn probability_of_fail(vin: &str) -> Value {
    let regex = Regex::new("^(?i)[A-HJ-NPR-Z0-9]{17}$").unwrap();
    let check_vin = regex.is_match(vin);
-    json!({"fuelUsage": check_vin})
+   let mut rng = rand::thread_rng();
+    if (check_vin) {
+        let fail_probability = Decimal::new(rng.gen_range(0..100), 2);
+        json!({
+            "failProbability": fail_probability,
+        })
+    } else {
+        json!({
+            "message": "Incorrect vin number!",
+        }) 
+    }
 }
 
 #[launch]
