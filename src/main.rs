c03c069e93f2ff48731f86f5354ea13e2bbc5e18
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-08-26 12:57:31 +0200

    initial serde json.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #[macro_use] extern crate rocket;
 
-#[get("/calculateDisselUsageForDistance")]
-fn dissel_usage() -> &'static str {
-    "Hello, world!"
+use rocket::serde::json::{json, Value};
+
+
+#[get("/calculateDisselUsageForDistance/<distance>/<year_of_production>/<fuel_usage_per_100km>")]
+fn dissel_usage(distance: u64,year_of_production: u16, fuel_usage_per_100km: u8) -> Value {
+    json!({"test": "test"})
 }
 
 #[launch]
