7b69f80be13cd8d334ee9bc66b8b100c409d5707
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-08-26 13:46:04 +0200

    Added EP proabilityOfUnitInjectorFail and added check_vin regex

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use] extern crate rocket;
 
 use rocket::serde::json::{json, Value};
-
+use regex::Regex;
 
 #[get("/calculateDisselUsageForDistance/<distance>/<year_of_production>/<fuel_usage_per_100km>")]
 fn dissel_usage(distance: u32,year_of_production: u32, fuel_usage_per_100km: u32) -> Value {
@@ -9,7 +9,15 @@ fn dissel_usage(distance: u32,year_of_production: u32, fuel_usage_per_100km: u32
     json!({"fuelUsage": fuel_usage})
 }
 
+#[get("/probabilityOfUnitInjectorFail/<vin>")]
+fn probability_of_fail(vin: &str) -> Value {
+   let regex = Regex::new("^(?i)[A-HJ-NPR-Z0-9]{17}$").unwrap();
+   let check_vin = regex.is_match(vin);
+    json!({"fuelUsage": check_vin})
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![dissel_usage])
+    rocket::build()
+    .mount("/", routes![dissel_usage, probability_of_fail])
 }
